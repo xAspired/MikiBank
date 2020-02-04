c8ccf588ca596cd082cecb2516e61d127f24ca8c
GianMattia <pinoeninfa@libero.it>
2020-02-04 13:06:47 +0100

    Created Main Class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] Args) {
+        List<String> contiBancari = new ArrayList<>();
+        File contiBancariFile = new File("contiBancari.txt");
+
+
+    }
+}
