56cc0127a50155262bd787d794d45bd2e519074e
GianMattia <pinoeninfa@libero.it>
2020-02-04 13:24:13 +0100

    Created the first txt File

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,33 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] Args) {
         List<String> contiBancari = new ArrayList<>();
-        File contiBancariFile = new File("contiBancari.txt");
 
+        /*
+            [Gestione file da parte dell'impiegato]
 
+            (Possibile rimozione dell'output o aggiunta di
+            eventuali metodi di sicurezza per gli impiegati)
+
+            Il file sarà creato solo alla prima esecuzione
+            del file. Dunque, le rimanenti volte basta verificarne
+            la presenza.
+         */
+
+        try {
+            File contiBancariFile = new File("contiBancari.txt");
+            if (contiBancariFile.createNewFile()) {
+                System.out.println("File creato: " + contiBancariFile.getName());
+            } else {
+                System.out.println("Il File esiste già.");
+            }
+        } catch (IOException e) {
+            System.out.println("Errore riscontrato.");
+            e.printStackTrace();
+        }
     }
 }
