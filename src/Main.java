146e2beb3e0b391f8b2db5bc6979ac933b4d1d22
GianMattia <pinoeninfa@libero.it>
2020-02-08 00:03:21 +0100

    Deleted some strings and code (Work in Progress..)

--- src/Main.java
+++ src/Main.java
@@ -2,77 +2,35 @@ import Packages.*;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] Args) throws Exception, ExceptionInInitializerError {
-        FileInputStream fileIn = new FileInputStream("contiCorrenti");
-        ObjectInputStream fileObjIn = new ObjectInputStream(fileIn);
-        System.out.print(fileObjIn.readObject());
+        ArrayList<contoCorrente> contiCorrentiArray = null;
+        File file = new File("contiCorrenti");
+        if (file.exists()) {
+            FileInputStream fileIn = new FileInputStream(file);
+            ObjectInputStream fileObjIn = new ObjectInputStream(fileIn);
 
-        ArrayList<contoCorrente> contiCorrentiArray = (ArrayList<contoCorrente>) fileObjIn.readObject();
+            contiCorrentiArray = (ArrayList<contoCorrente>) fileObjIn.readObject();
 
-        fileObjIn.close();
-        fileIn.close();
+            fileObjIn.close();
+            fileIn.close();
+        } else {
+            contiCorrentiArray = new ArrayList<contoCorrente>();
+        }
 
+        System.out.println("Size of List at start: "+contiCorrentiArray.size());
 
-//        int sceltaCliente;
-//        Scanner input = new Scanner(System.in);
-//        System.out.println("Buongiorno, cosa vuole fare?");
-//        System.out.println("[1] Creare un conto");
-//        System.out.println("[2] Visualizzare un conto");
-//        sceltaCliente = input.nextInt();
-//        switch (sceltaCliente) {
-//            case 1:
-//                //If che verifica che il tipo non abbia già un conto
-//
-//                creazioneCliente();
-//            default:
-//                System.out.print("Operazione non valida");
-//        }
-
-        contoCorrente c1 = new contoCorrente("IDNALFO", 14, 3);
+        contoCorrente c1 = new contoCorrente("IDNALFO", 14, 1);
         contoCorrente c2 = new contoCorrente("IDNALFO", 14, 3);
 
         contiCorrentiArray.add(c1);
 
-
-
-        /*
-         * =========================================================
-         * Serializzazione
-         * =========================================================
-         */
-        //Si crea il file di output e l'oggetto di output
-        FileOutputStream fileOut = new FileOutputStream("contiCorrenti");
+        FileOutputStream fileOut = new FileOutputStream(file);
         ObjectOutputStream fileObj = new ObjectOutputStream(fileOut);
 
-        //Scriviamo nel file oggetto l'arraylist
         fileObj.writeObject(contiCorrentiArray);
         fileObj.close();
         fileOut.close();
-
-
-
-
-    }
-
-
-    /*
-     * =========================================================
-     * Metodi Esterni al Main
-     * =========================================================
-     */
-    private static void creazioneCliente() {
-        int numBeneficiari = 1;
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Vuoi creare da solo un conto?");
-        System.out.println("[1] Sì  [2] No");
-        if(input.nextInt() == 1) {
-            System.out.print("Quanti beneficiari Le servono?");
-            numBeneficiari = input.nextInt();
-
-        }
     }
 }
