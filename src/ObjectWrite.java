b4e5a804ae42b93ec6a79c17db389959d10819ec
GianMattia <pinoeninfa@libero.it>
2020-02-10 16:20:39 +0100

    Added Basic info on Main Class

--- src/ObjectWrite.java
+++ src/ObjectWrite.java
@@ -1,14 +1,17 @@
+import Packages.contoCorrente;
+
 import java.io.*;
 import java.util.ArrayList;
-
-import Packages.*;
+import java.util.Scanner;
 
 /*
     Main Class
  */
 public class ObjectWrite {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        
+
         ArrayList<contoCorrente> contiCorrentiArray;
         File file = new File("contiCorrenti");
         if (file.exists()) {
@@ -25,10 +28,9 @@ public class ObjectWrite {
             contiCorrentiArray = new ArrayList<>();
         }
 
-        System.out.println("Dimensione dell'ArrayList: "+ contiCorrentiArray.size());
+        System.out.println("Dimensione dell'ArrayList: " + contiCorrentiArray.size());
 
         contoCorrente c1 = new contoCorrente("IDNALFO", 14, 1);
-        contoCorrente c2 = new contoCorrente("IDNALFO", 14, 3);
 
         contiCorrentiArray.add(c1);
 
@@ -38,6 +40,76 @@ public class ObjectWrite {
         fileObj.writeObject(contiCorrentiArray);
         fileObj.close();
         fileOut.close();
+
+        //Fine Serializzazione e Deserializzazione
+
+        int scelta = 0;
+        boolean whileStatement = true;
+
+
+        while (whileStatement) {
+            System.out.println("MIKIBANK - Oltre le Taglie");
+            System.out.println("[0] Uscita dal programma");
+            System.out.println("[1] Creazione di un conto corrente");
+            System.out.println("[2] Visualizzazione informazione conto corrente");
+            System.out.println("[3] Visualizzazione utente");
+            System.out.println("[4] Chiedere un prestito");
+            System.out.print("➡ ");
+
+            try {
+                scelta = Integer.parseInt(input.nextLine());
+                whileStatement = false;
+            }
+            catch (Exception ignored) {
+            }
+
+        }
+
+        /*
+            Switch - Scelta Azione
+         */
+        switch (scelta) {
+            case 0:
+                //Il Garbage Collector pulisce
+                System.out.print("Torni a Trovarci");
+                System.gc();
+                System.exit(0);
+                break;
+            case 1:
+                //Creazione Conto Corrente
+                creaConto(contiCorrentiArray);
+                break;
+            case 2:
+                //Visualizzare Info Conto Corrente
+                visualizzaInfoConto(contiCorrentiArray);
+                break;
+            case 3:
+                //Visualizza Info Utente
+                visualizzaInfoUtente(contiCorrentiArray);
+                break;
+            case 4:
+                //Chiedere un prestito
+                chiederePresito(contiCorrentiArray);
+                break;
+
+
+            default:
+                System.out.print("Azione non valida");
+                break;
+
+        }
+    }
+
+    private static void chiederePresito(ArrayList<contoCorrente> contiCorrentiArray) {
+    }
+
+    private static void visualizzaInfoUtente(ArrayList<contoCorrente> contiCorrentiArray) {
+    }
+
+    private static void visualizzaInfoConto(ArrayList<contoCorrente> contiCorrentiArray) {
+    }
+
+    private static void creaConto(ArrayList<contoCorrente> contiCorrentiArray) {
     }
 
 }
